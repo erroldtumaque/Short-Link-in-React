8152d229111d0277d54ff6826fc23dd2f128dd26
Errold Tumaque <errold.tumaque@gmail.com>
2018-02-27 23:23:07 -0800

    Fixed Sync Conflicts

--- server/main.js
+++ server/main.js
@@ -1,10 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
 import '../imports/api/users';
-<<<<<<< HEAD
 import '../imports/api/linkDB';
-=======
->>>>>>> origin/master
+
 
 Meteor.startup(() => {
   // code to run on server at startup
